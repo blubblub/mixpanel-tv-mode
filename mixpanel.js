7b2edc8fc168d868ae420f0f1230a557ddbc16da
Dal Rupnik <legoless@gmail.com>
2023-07-10 10:54:33 +0200

    Add hiding of top bars

--- mixpanel.js
+++ mixpanel.js
@@ -1,7 +1,23 @@
-const topHeader = document.querySelector('div.mp-home-header');
-
-console.log('topHeader', topHeader);
+const topHeader = document.querySelector('#wrapper').firstChild.nextSibling;
 
 if (topHeader) {
     topHeader.style.display = 'none';
 }
+
+const topIntroHeader = document.querySelector('div.header');
+
+if (topIntroHeader) {
+    topIntroHeader.style.display = 'none';
+}
+
+function updateValue() {
+    const valueWrapper = document.querySelector('.metric-container');
+
+    console.log('Yestsso');
+    console.log('value', valueWrapper);
+    if (valueWrapper) {
+        valueWrapper.style.display = 'none';
+    }
+}
+
+setTimeout(updateValue, 1500);
