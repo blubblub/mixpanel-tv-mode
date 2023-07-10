c636645eb1d036a05709163de03c0b475ce0b829
Dal Rupnik <legoless@gmail.com>
2023-07-10 13:01:21 +0200

    Code cleanup

--- mixpanel.js
+++ mixpanel.js
@@ -1,41 +1,75 @@
-const topHeader = document.querySelector('#wrapper').firstChild.nextSibling;
+var hideSearchBar = false;
+var hideDescriptionBar = false;
+var displayConcreteNumbers = false;
 
-if (topHeader) {
-    topHeader.style.display = 'none';
-}
+// Fetch options
+chrome.storage.sync.get(
+{
+    hideSearchBar: true,
+    hideDescriptionBar: true,
+    displayConcreteNumbers: true
+},
+(items) => {
+    hideSearchBar = items.hideSearchBar;
+    hideDescriptionBar = items.hideDescriptionBar;
+    displayConcreteNumbers = items.displayConcreteNumbers;
+});
 
-const topIntroHeader = document.querySelector('div.header');
+function updateMetric(multiMetricSelectorChart) {
+    if (multiMetricSelectorChart.shadowRoot != null) {
+        const multiMetricSelector = multiMetricSelectorChart.shadowRoot.querySelector('mp-multi-metric');
 
-if (topIntroHeader) {
-    topIntroHeader.style.display = 'none';
-}
-
-function updateValue() {
-    //const valueWrapper = document.querySelectorAll('div.card-report');
-    const valueWrapper = document.querySelectorAll('mp-insights-metric-chart');
-
-    console.log('Yestsso');
-    console.log('value', valueWrapper);
-    if (valueWrapper.length > 0) {
-        const valueSelector = valueWrapper[0].shadowRoot.querySelector('mp-multi-metric');
-        //valueWrapper[0].style.display = 'none';
-
-        console.log('valueS', valueSelector);
-
-        if (valueSelector) {
-            const valueMetric = valueSelector.shadowRoot.querySelector('mp-metric');
+        if (multiMetricSelector) {
+            const metricSelector = multiMetricSelector.shadowRoot.querySelector('mp-metric');
             
-            if (valueMetric) {
-                const valueTooltip = valueMetric.getAttribute('value-tooltip');
-                const value = valueMetric.shadowRoot.querySelector('div.value');
-                const valueText = value.textContent;
+            if (metricSelector) {
+                const valueTooltip = metricSelector.getAttribute('value-tooltip');
+                const value = metricSelector.shadowRoot.querySelector('div.value');
                 value.innerHTML = valueTooltip;
                 value.style.fontSize = '4vw';
-
-                console.log('valueText', valueText);
             }
         }
     }
 }
 
-setTimeout(updateValue, 2000);
+function updateMetrics() {
+    const allMetricCharts = document.querySelectorAll('mp-insights-metric-chart');
+
+    // Fetch all metrics and try to update
+    for (let i = 0; i < allMetricCharts.length; i++) {
+        updateMetric(allMetricCharts[i]);
+    }
+}
+
+function hideDescriptionBar() {
+    const topIntroHeader = document.querySelector('div.header');
+
+    if (topIntroHeader) {
+        topIntroHeader.style.display = 'none';
+    }    
+}
+
+function hideSearchBar() {
+    const topHeader = document.querySelector('#wrapper').firstChild.nextSibling;
+
+    if (topHeader) {
+        topHeader.style.display = 'none';
+    }    
+}
+
+function update() {
+
+    if (hideSearchBar) {
+        hideSearchBar();
+    }
+
+    if (hideDescriptionBar) {
+        hideDescriptionBar();
+    }
+    
+    if (displayConcreteNumbers) {
+        updateMetrics();
+    }
+}
+
+setTimeout(update, 500);
